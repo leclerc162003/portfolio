8585c58c8c2beef2b7b50eef096b1c3327b40299
Hong <lewischua.junhong@gmail.com>
2026-05-15 22:47:00 +0800

    edited bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,11 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://astronaut.github.io',
   base: '/portfolio',
+  fonts: [
+    {
+      provider: fontsource(),
+      name: "Atkinson Hyperlegible",
+      cssVariable: "--font-atkinson",
+    },
+  ],
 })
