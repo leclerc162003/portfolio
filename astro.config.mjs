7d5ee2bfa55c7fface2e833099d0131183eff9eb
Hong <lewischua.junhong@gmail.com>
2026-05-15 22:38:31 +0800

    added astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://leclerc162003.github.io',
+  site: 'https://astronaut.github.io',
+  base: '/portfolio',
 })
